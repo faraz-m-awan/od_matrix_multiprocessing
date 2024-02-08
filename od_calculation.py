a319f23722c2e4ad0d66dea7a7464834066c1857
Faraz Malik Awan <FarazMalik.Awan@glasgow.ac.uk>
2024-02-08 12:50:16 +0000

    Modified Data Fetching

--- od_calculation.py
+++ od_calculation.py
@@ -23,167 +23,130 @@ import matplotlib.pyplot as plt
 
 from datetime import datetime
 import concurrent.futures
+from concurrent.futures import ThreadPoolExecutor
 import multiprocessing
-####################################################
 
+####################################################
+#                                                  #
 #         Customized Modules                       #
-
+#                                                  #
 ####################################################
-from data_load import fetchDataFromDB, fetchData
+from data_load import fetchData
 from impression_filtering import getFilteredData,filter_data_process
 from stop_node_detection import getStopNodes, stop_node_process
 from flow_generation import generateFlow, processFlowGenration
-
-###########################################
-
-
-
-
-
 
 
 
 class ODCalculation():
 
-    year=2020
-    month=[i for i in range(1,13)] #[i for i in range(1,13)] month number | ['all']
+    year=2019
+    month=['all'] #[i for i in range(1,13)] month number | ['all']
     radius=500
     time_th=5
-    impr_acc=500
+    impr_acc=100
     cpu_cores=8 # Cores to be used for multiprocessing
 
-
     def __init__(self_):
-
-        
-
-
         return
 
-    def getLoadBalancedBuckets(self_,tdf,bucket_size):
+    def getLoadBalancedBuckets(self_,tdf,bucket_size): # Multiprocessing is being used for processing the data for Stop node detection and flow generation. This Funcition devides the data based on the UID and Number of Impressions in a way that load on every process is well-balanced.
         
         print(f'{datetime.now()}: Getting unique users')
-        unique_users=tdf['uid'].unique()
+        unique_users=tdf['uid'].unique() # Getting Unique Users in the data
         
         print(f'{datetime.now()}: Creating sets')
-        num_impr_df=pd.DataFrame(tdf.groupby('uid').size(),columns=['num_impressions']).reset_index().sort_values(by=['num_impressions'],ascending=False)
+        num_impr_df=pd.DataFrame(tdf.groupby('uid').size(),columns=['num_impressions']).reset_index().sort_values(by=['num_impressions'],ascending=False) # Creating a DataFrame containing Unique UID and Total number of impressions that Unique UID has in the data.
       
 
-        buckets={}
-        bucket_sums={}
+        buckets={} # A dictionary containing buckets of UIDs. Each bucket represent the CPU core. This dictionary tells how many users' data will be process on which core. For example, if bucket 1 contains 10 UIDs, data of those 10 UIDs will be processed on the core 1.
+        bucket_sums={} # A flag dictionary to keep the track of load on each bucket.
         
         for i in range (1,bucket_size+1):
-            buckets[i]=[]
-            bucket_sums[i]=0
+            buckets[i]=[] # Initializing empty buckets
+            bucket_sums[i]=0 # Load in each bucket is zero initially
 
         
         # Allocate users to buckets
         for _, row in num_impr_df.iterrows():
-            user, impressions = row['uid'], row['num_impressions']
+            user, impressions = row['uid'], row['num_impressions'] # Getting the UID and the number of impressions of that UID
             # Find the bucket with the minimum sum of impressions
-            min_bucket = min(bucket_sums, key=bucket_sums.get)
+            min_bucket = min(bucket_sums, key=bucket_sums.get) # Getting the bucket with the minimum load. Initially, all the buckets have zero load.
             # Add user to this bucket
-            buckets[min_bucket].append(user)
+            buckets[min_bucket].append(user) # Adding UID to the minimum bucket
             # Update the sum of impressions for this bucket
-            bucket_sums[min_bucket] += impressions
+            bucket_sums[min_bucket] += impressions # Updating the load value of the bucket. For example, UID 1 was added to Bucket 1 and UID 1 had 1000 impressions (records). So, load of bucket 1 is 1000 now. 
 
 
         print(f'{datetime.now()}: Creating seperate dataframes')
 
-        tdf_collection=[]
+        tdf_collection=[] # List of collection of DataFrames. This list will contain the number of DataFrames=number of CPU Cores. Each DataFrame will be processed in a seperate core as a seperate process.
         for i in range (1, bucket_size+1):
             tdf_collection.append(tdf[tdf['uid'].isin(buckets[i])].copy())
 
         return tdf_collection
     
 
-    def getDatesDivision(self_,month):
-
-        args=[]
-        window_size=3
-        start_month=month
-        end_month=month
-        start_date=1
-        end_date=start_date+window_size
-        start_year=obj.year
-        end_year=obj.year
-
-        for i in range(0,self_.cpu_cores):
-            
-            args.append((start_year,end_year,start_month,end_month,start_date,end_date))
-
-            if i==0:
-                window_size=4
-
-            if i==6:
-                
-                if start_month==12:
-
-                    end_year=end_year+1
-                    end_month=1
-
-                    start_date=end_date
-                    end_date=1
-            
-                else:    
-                    
-                    end_month+=1
-                    start_date=end_date
-                    end_date=1
-            
-
-            else:
-                start_date=end_date
-                end_date=start_date+window_size
-
-
-
-        return args
     
-    def getMonthlyDivsion(self_):
 
+    def getQueriesForAllYearProcessing(self_,year): 
 
-        args=[]
-        window_size=2
-        start_year=self_.year
-        end_year=self_.year 
-        start_month=1   # 2019-01-01
-        end_month=start_month+window_size
-        start_date=1
-        end_date=1
-        
+        query=[]
 
-        for i in range(1,7): # Using only 6 cores
-
-            if i==1:
-                start_year=self_.year
-                end_year=self_.year 
-                
-                start_month=1  
-                end_month=start_month+window_size
-
-                start_date=1
-                end_date=1
-            elif i==6:
-                
-                start_month=end_month
-                end_month=1
-                end_year+=1
+        for month in range(1,13,2): # This loop will run 6 times. Each loop will generate a query for fetching data for two months
+            if month==11:
+                query.append(
+                    f"""
+                    SELECT timestamp as datetime, device_iid_hash as uid, impression_lat as lat, impression_lng as lng, impression_acc
+                    FROM by_year.huq_gla_{year}_v1_2
+                    WHERE timestamp >= '{year}-{month:02d}-01' and timestamp <'{year+1}-{1:02d}-01'
+                    """
+                    )
             else:
-                start_month=end_month
-                end_month=start_month+window_size
-            
-            args.append((start_year,end_year,start_month,end_month,start_date,end_date))
+                query.append(
+                    f"""
+                    SELECT timestamp as datetime, device_iid_hash as uid, impression_lat as lat, impression_lng as lng, impression_acc
+                    FROM by_year.huq_gla_{year}_v1_2
+                    WHERE timestamp >= '{year}-{month:02d}-01' and timestamp <'{year}-{month+2:02d}-01'
+                    """
+                    )
+                    
+        return query
+    def getQueriesForMonthlyProcessing(self_,year,month):
 
-            
+        query=[]
 
+        for day in range(1,31,5): # We are using 6 threads to fetch the data. Each month will be devided into 5 date windows. Each Thread will be responsible for fetching data for its own date window.
 
-        return args
+            if day==26:
+                if month==12:
+                    query.append(
+                    f"""
+                    SELECT timestamp as datetime, device_iid_hash as uid, impression_lat as lat, impression_lng as lng, impression_acc
+                    FROM by_year.huq_gla_{year}_v1_2
+                    WHERE timestamp >= '{year}-{month:02d}-{day:02d}' and timestamp <'{year+1}-01-01'
+                    """
+                    )
+                else:
+                    query.append(
+                    f"""
+                    SELECT timestamp as datetime, device_iid_hash as uid, impression_lat as lat, impression_lng as lng, impression_acc
+                    FROM by_year.huq_gla_{year}_v1_2
+                    WHERE timestamp >= '{year}-{month:02d}-{day:02d}' and timestamp <'{year}-{month+1:02d}-01'
+                    """
+                    )
+            else:
+                query.append(
+                    f"""
+                    SELECT timestamp as datetime, device_iid_hash as uid, impression_lat as lat, impression_lng as lng, impression_acc
+                    FROM by_year.huq_gla_{year}_v1_2
+                    WHERE timestamp >= '{year}-{month:02d}-{day:02d}' and timestamp <'{year}-{month:02d}-{day+5:02d}'
+                    """
+                    )
+
+        return query
 
     def saveFile(self_,path,fname,df):
-        
-        #path=f'D:\Mobile Device Data\OD_calculation_latest_work\HUQ_OD\\{year}\\na_flows'
-        #fname=f'na_flows_{radius}_{year}.csv'
 
         if not os.path.exists(path):
             os.makedirs(path)
@@ -195,14 +158,11 @@ class ODCalculation():
    
         
 
-    
-
-
-
-
 if __name__=='__main__':
     
+    
     obj=ODCalculation()
+    
     for month in obj.month:
 
         print(f"""
@@ -215,61 +175,54 @@ if __name__=='__main__':
 
         start_time=datetime.now()
 
+        
+        ##################################################################################
+        #                                                                                #
+        #                           Fetching Data From DB                                #
+        #                                                                                #
+        ##################################################################################
+
         print(f'{start_time}: Fetching data from Database')
         
-            
-
-        args=[]
 
         if month=='all':
-            args=obj.getMonthlyDivsion()
-            
-            print(f'{args}')
-            with multiprocessing.Pool(6) as pool:
-                results = pool.starmap(fetchData, args)
-
-            print(f'{datetime.now()}: Data Concatination')
-            result1, result2, result3, result4,result5, result6 = results
-            
-            obj.raw_df=pd.concat([result1, result2, result3, result4,result5, result6])
-            print(f'{datetime.now()}: Data Concatination Completed')
+            print('Yearly Processing')
+            query=obj.getQueriesForAllYearProcessing(obj.year)
         else:
-            args=obj.getDatesDivision(month)
-            print(f'{args}')
-            with multiprocessing.Pool(obj.cpu_cores) as pool:
-                results = pool.starmap(fetchData, args)
-
-            print(f'{datetime.now()}: Data Concatination')
-            result1, result2, result3, result4,result5, result6, result7, result8 = results
-            
-            obj.raw_df=pd.concat([result1,result2,result3,result4,result5,result6,result7,result8])
-            print(f'{datetime.now()}: Data Concatination Completed')
-
-
+            print('Monthly Processing')
+            query=obj.getQueriesForMonthlyProcessing(obj.year,month)
         
+     
+        with ThreadPoolExecutor(max_workers=6) as executor:
+            results = list(executor.map(fetchData, query))
 
-        
-        
-        #obj.raw_df=pd.concat([future1.result(),future2.result(),future3.result(),future1.result()])
+        print(f'{datetime.now()}: Data Concatination')
+        obj.raw_df=pd.concat(results)
+        print(f'{datetime.now()}: Data Concatination Completed')
+
         print(f'{datetime.now()}: Data fetching completed\n\n')
         print(f'Number of Records: {obj.raw_df.shape[0]}')
+    
 
         # Converting Raw DataFrame into a Trajectory DataFrame
-        
         traj_df= TrajDataFrame(obj.raw_df, latitude='lat',longitude='lng',user_id='uid',datetime='datetime') # Coverting raw data into a trajectory dataframe
-
-
         tdf_collection= obj.getLoadBalancedBuckets(traj_df,obj.cpu_cores)
 
-        print(f'{datetime.now()}: Filtering Started')
 
+        ##################################################################################
+        #                                                                                #
+        #                           Filtering Data Based on                              #
+        #              Impression Accuracy and Speed Between GPS Points                  #
+        #                                                                                #
+        ##################################################################################
+
+        print(f'{datetime.now()}: Filtering Started')
         args=[(tdf,obj.impr_acc) for tdf in tdf_collection]
         with multiprocessing.Pool(obj.cpu_cores) as pool:
             results = pool.starmap(filter_data_process, args)
 
         result1, result2, result3, result4,result5, result6, result7, result8 = results
         traj_df=pd.concat([result1,result2,result3,result4,result5,result6,result7,result8])
-
         print(f'{datetime.now()}: Filtering Finished\n\n\n')
 
         ##################################################################################
@@ -279,33 +232,24 @@ if __name__=='__main__':
         ##################################################################################
 
         print(f'{datetime.now()}: Stop Node Detection Started\n\n')
-
         print(f'Detecting stop nodes for the month: {traj_df.datetime.dt.month.unique().tolist()}')
         print(f'Radius: {obj.radius}\nTime Threshold: {obj.time_th}\nImpression Accuracy: {obj.impr_acc}')
-        
         tdf_collection= obj.getLoadBalancedBuckets(traj_df,obj.cpu_cores)
-
         print(f'{datetime.now()}: Stop Node Detection Started')
-
         args=[(tdf,obj.time_th,obj.radius) for tdf in tdf_collection]
         with multiprocessing.Pool(obj.cpu_cores) as pool:
             results = pool.starmap(stop_node_process, args)
 
         result1, result2, result3, result4,result5, result6, result7, result8 = results
-
         stdf=pd.DataFrame(pd.concat([result1,result2,result3,result4,result5,result6,result7,result8]))
-
-        
-
         print(f'{datetime.now()} Stop Node Detection Completed\n')
-
-        #fname=f'D:\Mobile Device Data\OD_calculation_latest_work\HUQ_OD\\{obj.year}\\huq_stop_nodes_{obj.year}_{month}_{obj.radius}m_{obj.time_th}min_{obj.impr_acc}m.csv'
-
-        #stdf.to_csv(fname,index=False)
-
+        
+        # Saving Stop Nodes
         obj.saveFile(
             path=f'D:\Mobile Device Data\OD_calculation_latest_work\HUQ_OD\\{obj.year}\\stop_nodes',
             fname=f'huq_stop_nodes_{obj.year}_{month}_{obj.radius}m_{obj.time_th}min_{obj.impr_acc}m.csv',
+            #path=f'D:\Mobile Device Data\OD_calculation_latest_work\HUQ_OD\\validation',
+            #fname=f'val_stop_nodes_{obj.radius}m_{obj.year}.csv',
             df=stdf
         )
         
@@ -316,13 +260,7 @@ if __name__=='__main__':
         #                           Flow Generation                                      #
         #                                                                                #
         ##################################################################################
-        #print('Starting')
-        #stdf=pd.read_csv(f'D:\Mobile Device Data\OD_calculation_latest_work\HUQ_OD\\{obj.year}\\stop_nodes\\huq_stop_nodes_{obj.year}_{month}_{obj.radius}m_5min_100m.csv',parse_dates=['datetime','leaving_datetime']) #Delete
-        #temp_users=list(pd.read_csv('D:\Mobile Device Data\OD_calculation_latest_work\HUQ_OD\\2022\\test_users.csv')['uid'].values) #Delete
-
-        #stdf=stdf[stdf['uid'].isin(temp_users)] #Delete
-        #obj.raw_df=obj.raw_df[obj.raw_df['uid'].isin(temp_users)] #Delete
-
+ 
 
         stdf.rename(columns={'lat':'org_lat','lng':'org_lng'},inplace=True)
         stdf['dest_at']=stdf.groupby('uid')['datetime'].transform(lambda x: x.shift(-1))
@@ -332,50 +270,34 @@ if __name__=='__main__':
 
         #Indexing Raw Data
         obj.raw_df.set_index(['uid','datetime'],inplace=True)
-        obj.raw_df.sort_index(inplace=True)
-
-
-
-
-        
-
-        
+        obj.raw_df.sort_index(inplace=True)      
         tdf_collection= obj.getLoadBalancedBuckets(stdf,obj.cpu_cores)
-
+        
         print(f'{datetime.now()}: Generating args')
         args=[]
         #[(tdf,obj.raw_df[obj.raw_df['uid'].isin(tdf['uid'].unique())]) for tdf in tdf_collection]
         for tdf in tdf_collection:
-            args.append(
-                (
-                    tdf,
-                    obj.raw_df.copy()
-                )
-            )
+            args.append((tdf,obj.raw_df.copy()))
+
         print(f'{datetime.now()}: args Generation Completed')
-
-
         print(f'{datetime.now()}: Flow Generation Started\n\n')
         with multiprocessing.Pool(obj.cpu_cores) as pool:
             results = pool.starmap(generateFlow, args)
 
         result1, result2, result3, result4,result5, result6, result7, result8 = results
-
         flow_df=pd.concat([result1,result2,result3,result4,result5,result6,result7,result8])
-
         print(f'{datetime.now()} Flow Generation Completed\n')
 
-        #flow_df.to_csv(f'D:\Mobile Device Data\OD_calculation_latest_work\HUQ_OD\\{obj.year}\\trips\huq_trips_{obj.year}_{month}_{obj.radius}m_{obj.time_th}min_{obj.impr_acc}m.csv',index=False)
-
-
+        # Saving Flow
         obj.saveFile(
-            path=f'D:\Mobile Device Data\OD_calculation_latest_work\HUQ_OD\\{obj.year}\\trips',
-            fname=f'huq_trips_{obj.year}_{month}_{obj.radius}m_{obj.time_th}min_{obj.impr_acc}m.csv',
+            #path=f'D:\Mobile Device Data\OD_calculation_latest_work\HUQ_OD\\{obj.year}\\trips',
+            #fname=f'huq_trips_{obj.year}_{month}_{obj.radius}m_{obj.time_th}min_{obj.impr_acc}m.csv',
+            path=f'D:\Mobile Device Data\OD_calculation_latest_work\HUQ_OD\\validation',
+            fname=f'val_trips_{obj.radius}m_{obj.year}.csv',
             df=flow_df
         )
 
         end_time=datetime.now()
-
         print(f'{end_time}: Processed Completed')
         print(f'\n\nTotal Time Taken: {(end_time-start_time).total_seconds()/60} minutes')
         ##################################################################################
@@ -383,11 +305,4 @@ if __name__=='__main__':
         #                           Trips Extrapolation                                  #
         #                                                                                #
         ##################################################################################
-
-
-
-
-
-        
-
         
