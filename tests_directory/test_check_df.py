d20ac4ba6c0073cb7d718a4e7d4bb6ccc4e75c30
Faraz Malik Awan <FarazMalik.Awan@glasgow.ac.uk>
2024-03-25 23:24:47 +0000

    Added Unit Test and Report.ipynb

---
+++ tests_directory/test_check_df.py
@@ -0,0 +1,13 @@
+import unittest
+
+class TestOD(unittest.TestCase):
+    def test_check_df(self):
+        assert 1==2
+    def test_foo(self):
+        assert 1==1
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
